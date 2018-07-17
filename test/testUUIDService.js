b44334cf5e42b040be8ef3234489f124061b6c7b
dhrubo <dhruba.bhattacharjee@in.ibm.com>
2018-07-17 17:51:12 +0530

    correcting unit test case a bit.. lot to go

--- test/testUUIDService.js
+++ test/testUUIDService.js
@@ -1,26 +1,34 @@
 /**
  * This is unit testcase implementation for all functions of uuidService.js
  * 
- * @author Dhruo
+ * @author Dhrubo
  * @license MIT
  */
 
- 'use strict'
+'use strict'
 
 var expect = require('chai').expect;
 
 const UUIDService = require('../UUIDService.js');
 let service = new UUIDService();
+const uuid1 = require('uuid/v1');
+const uuid4 = require('uuid/v4');
 
 describe('Testing UUID Service', function(){
     it('it shpould return type version 1 UUID',function(){
-        var uuid = service.getUUIDByVersion(service.V1).toString();  
+        var uuid = service.getUUIDByVersion(service.V1).toString();        
+        var localuuid = uuid1();
+        expect(uuid).not.equals(localuuid);
     }) 
     it('it shpould return type version 4 UUID',function(){
-        var uuid = service.getUUIDByVersion(service.V4).toString();   
+        var uuid = service.getUUIDByVersion(service.V4).toString();        
+        var localuuid = uuid4();
+        expect(uuid).not.equals(localuuid);
     })
     it('it shpould return type V4 UUID by default',function(){
-        var uuid = service.getUUID().toString();       
+        var uuid = service.getUUID().toString();        
+        var localuuid =uuid4();
+        expect(uuid).not.equals(localuuid);
     }) 
 });
 
