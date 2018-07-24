9b9d9002203e851c5fbdc006bd4d225ab8eeec55
dhrubo <dhruba.bhattacharjee@in.ibm.com>
2018-07-24 15:48:33 +0530

    adding required files in main js file

--- CommonServices.js
+++ CommonServices.js
@@ -7,3 +7,6 @@
  */
 
  var log = require('./LoggingService.js');
+ var checksum = require('./ChecksumService.js');
+ var uuid = require('./UUIDService.js');
+ var numberFormatter = require('./NumberFormatter.js');
